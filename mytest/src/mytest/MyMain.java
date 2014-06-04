03784bd35a1f0f4d97a43ac902f2f5d6bc92796a
git <dcb2002@163.com>
2014-06-04 10:29:23 +0800

    com111

---
+++ mytest/src/mytest/MyMain.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package mytest;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MyMain {
+
+	/**
+	 * 
+	 */
+	public MyMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+	}
+
+}
