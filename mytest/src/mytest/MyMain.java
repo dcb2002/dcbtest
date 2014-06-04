1a6e44f895d891dd0e269cec68123be7fc7998b5
git <dcb2002@163.com>
2014-06-04 11:23:19 +0800

    ccc-dve

--- mytest/src/mytest/MyMain.java
+++ mytest/src/mytest/MyMain.java
@@ -21,8 +21,8 @@ public class MyMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// TODO Auto-generated method 123
-
+		// TODO dev method dev
+		//dev
 	}
 
 }
