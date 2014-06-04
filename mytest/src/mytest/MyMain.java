aa0d6bb11051b4e6c1b92fc875c22cd65a86d216
git <dcb2002@163.com>
2014-06-04 11:17:23 +0800

    ccc123

--- mytest/src/mytest/MyMain.java
+++ mytest/src/mytest/MyMain.java
@@ -21,7 +21,7 @@ public class MyMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method 123
 
 	}
 
